8234db14b65e5bad42616fdf05c32d09f07cfb62
Tramp 87 <tramp87@protonmail.com>
2018-08-19 03:21:21 +0000

    outline steps for trampoline

--- augur-trampoline/src/Transaction.js
+++ augur-trampoline/src/Transaction.js
@@ -9,6 +9,14 @@ const networks = {
   mainnet: 1,
 };
 
+// Steps here:
+// 1. Connect to Ethereum network
+// 2. Choose account
+// 3. Fetch data and display it to user
+// 4. Sign
+// 5. Send transaction and wait for confirmation
+// 6. Redirect back
+
 const Transaction = ({ match }: { match: * }) => {
   const networkID = nullthrows(networks[match.params.network]);
   return (
